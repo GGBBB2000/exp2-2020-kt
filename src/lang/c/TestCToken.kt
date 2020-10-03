d2c88ab1c75b97212b78a3cb20ff6e914e70d8db
GGBBB2000 <ggbbb@mineo.jp>
2020-10-03 20:40:00 +0900

    TokenizerのテストをKotlin化

---
+++ src/lang/c/TestCToken.kt
@@ -0,0 +1,55 @@
+package lang.c
+
+import lang.FatalErrorException
+import lang.IOContext
+
+object TestCToken {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val inFile = args[0] // 適切なファイルを絶対パスで与えること
+        val ioCtx = IOContext(inFile, System.out, System.err)
+        val tokenizer = CTokenizer(CTokenRule())
+        val pcx = CParseContext(ioCtx, tokenizer)
+        try {
+            val ct = pcx.tokenizer
+            val tk = ct.getNextToken(pcx)
+            if (TestTokenizer.isFirst(tk)) {
+                TestTokenizer(pcx).apply {
+                    parse(pcx)
+                    codeGen(pcx)
+                }
+            }
+        } catch (e: FatalErrorException) {
+            e.printStackTrace()
+        }
+    }
+
+    private class TestTokenizer  //		program  ::= { token } EOF
+    (pcx: CParseContext?) : CParseRule() {
+        override fun parse(ctx: CParseContext) {
+            var tk = ctx.tokenizer.getCurrentToken(ctx)
+            while (tk.type != CToken.TK_EOF) {
+                if (tk.type == CToken.TK_NUM) {
+                    ctx.ioContext.outStream.println("Token=" + tk.toExplainString() + "type=" + tk.type + " valule=" + tk.intValue)
+                } else {
+                    ctx.ioContext.outStream.println("Token=" + tk.toExplainString() + "type=" + tk.type)
+                }
+                tk = ctx.tokenizer.getNextToken(ctx)
+            }
+        }
+
+        @Throws(FatalErrorException::class)
+        override fun semanticCheck(pcx: CParseContext) {
+            // do nothing
+        }
+
+        @Throws(FatalErrorException::class)
+        override fun codeGen(pcx: CParseContext) {
+            // do nothing
+        }
+
+        companion object {
+            fun isFirst(tk: CToken?): Boolean = true
+        }
+    }
+}
